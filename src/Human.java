a15c552df2bd04e77bcf2cea8718420ff849c082
Студент НИУ ВШЭ <Student@POKR-R601-STD07.staff.corp.local>
2023-07-03 14:50:34 +0300

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,11 @@
+public class Human {
+    int age;
+    String name;
+    String surname;
+
+    Human(String name, String surname, int age){
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,12 @@
+public class Point {
+    float x;
+    float y;
+
+    Point(float x, float y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+
+}
