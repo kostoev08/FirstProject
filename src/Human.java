c8e6a641e680776773a8ff384c8b7f0999127429
Student <kostoevmansur670@gmail.com>
2023-07-03 16:29:05 +0300

    Боба

--- src/Human.java
+++ src/Human.java
@@ -1,11 +1,17 @@
 public class Human {
     int age;
-    String name;
-    String surname;
+    String FIO;
+    float capital;
 
-    Human(String name, String surname, int age){
+    Human(String FIO, float capital, int age){
         this.age = age;
-        this.name = name;
-        this.surname = surname;
+        this.FIO = FIO;
+        this.capital = capital;
+    }
+    void work(int DaysOfWork){
+        capital = capital + 2000 * DaysOfWork;
+    }
+    void walk(int DaysOfWalk){
+        capital = capital - 2000 * DaysOfWalk;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jgjgj");
+    Human mus = new Human("Kosoev Muslim Kazb", 50000, 17);
+    Bank Alfa = new Bank(1000000000);
+
+    mus.work(32);
+    mus.walk(6);
+
+    Alfa.save(10000, mus, 10);
+    System.out.println(mus.capital);
+    System.out.println(mus.age);
+
     }
 }
